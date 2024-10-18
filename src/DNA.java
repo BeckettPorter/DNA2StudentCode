9b37c35d204b9d07a61acadfb58d95a06d8a39d2
BeckettPorter <porterbeckett@gmail.com>
2024-10-18 10:16:56 -0700

    Changed all the code to use hashing, hashNextLetter does not work :(

--- src/DNA.java
+++ src/DNA.java
@@ -13,38 +13,144 @@
 
 public class DNA
 {
+    public static final int RADIX = 4;
+    public static final long largePrime =   2147483647;
+
     public static int STRCount(String sequence, String STR)
     {
-        // Instance variables, I just made the STR length a final variable here because it will never be changed.
-        final int strLength = STR.length();
-        int index = 0;
+
+
+        return searchHashes(sequence, STR);
+
+    }
+
+
+
+
+
+
+    private static int searchHashes(String sequence, String target)
+    {
+        long targetHash = hashSingleString(target);
         int currentNumFound = 0;
         int maxFound = 0;
-        char firstSTRLetter = STR.charAt(0);
+        long currentHash = hashSingleString(sequence.substring(0, target.length()));
+        int targetLength = target.length();
 
-        // While loop that runs as long as the index is within the length of the sequence.
-        while (index <= sequence.length() - strLength)
+        for (int i = 0; i < sequence.length() - target.length(); i++)
         {
-            // Only bother checking for the STR if sequence at the current index is the same as the start of the STR.
-            if (sequence.charAt(index) == firstSTRLetter && STR.equals(sequence.substring(index, index + strLength)))
+            currentHash = hashNextLetter(sequence.charAt(0), sequence.charAt(i + targetLength), currentHash, i);
+
+            if (currentHash == targetHash)
             {
-                // If incrementing the current number of STRs found in a row would be greater than the current max
-                // number found in a row, set this new amount to the max found.
                 if (++currentNumFound > maxFound)
                 {
                     maxFound = currentNumFound;
                 }
-                // Skip ahead by strLength to avoid unnecessary checks.
-                index += strLength;
+                i += target.length() - 1;
             }
-            // If the check for the STR failed, just set the current number of sequences found in a row to 0
-            // and increment index.
             else
             {
                 currentNumFound = 0;
-                index++;
             }
         }
+
         return maxFound;
     }
+
+    private static long hashNextLetter(char oldChar, char nextChar, long currentHash, int index)
+    {
+        int oldCharVal = mapChar(oldChar);
+        int nextCharVal = mapChar(nextChar);
+
+        // Remove the hash value for the first char.
+        // TODO: Fix this, need to do radix power thing somehow?
+        long adjustedRadix = (long) Math.pow((double) currentHash, index);
+        currentHash = ((currentHash + largePrime) - oldCharVal * adjustedRadix % largePrime) % largePrime;
+
+        // Then add the next char
+        currentHash = ((currentHash * RADIX) + nextCharVal) % largePrime;
+
+        return currentHash;
+    }
+
+    private static long hashSingleString(String str)
+    {
+        long hash = 0;
+
+        for (int i = 0; i < str.length(); i++)
+        {
+            hash = (RADIX * hash + mapChar(str.charAt(i))) % largePrime;
+        }
+        return hash;
+    }
+
+    private static int mapChar(char c)
+    {
+        switch (c)
+        {
+            case 'A':
+                return 0;
+            case 'C':
+                return 1;
+            case 'G':
+                return 2;
+            case 'T':
+                return 3;
+        }
+        return -1;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // While loop that runs as long as the index is within the length of the sequence.
+//        while (index <= sequence.length() - strLength)
+//        {
+//            // Only bother checking for the STR if sequence at the current index is the same as the start of the STR.
+//            if (sequence.charAt(index) == firstSTRLetter && STR.equals(sequence.substring(index, index + strLength)))
+//            {
+//                // If incrementing the current number of STRs found in a row would be greater than the current max
+//                // number found in a row, set this new amount to the max found.
+//                if (++currentNumFound > maxFound)
+//                {
+//                    maxFound = currentNumFound;
+//                }
+//                // Skip ahead by strLength to avoid unnecessary checks.
+//                index += strLength;
+//            }
+//            // If the check for the STR failed, just set the current number of sequences found in a row to 0
+//            // and increment index.
+//            else
+//            {
+//                currentNumFound = 0;
+//                index++;
+//            }
+//        }
+//        return maxFound;
+//    }
 }
